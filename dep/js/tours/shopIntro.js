be79d3c4fa96b543c0b086e30749a67ae78bd0a6
baharalidurrani <baharalidurrani@gmail.com>
2020-05-14 14:53:44 -0700

    introjs added for shop

---
+++ dep/js/tours/shopIntro.js
@@ -0,0 +1,35 @@
+console.log("intro loaded....");
+// Instance the tour
+const steps = [
+  {
+    element: "#PolarisTextField1",
+    title: "Title of my step 1",
+    content: "Content of my step 1",
+    placement: "auto"
+  },
+  {
+    element: "html",
+    title: "Title of my step 2",
+    content: "Content of my step 2",
+    placement: "auto"
+  },
+  {
+    element: "#AdjustQuantityPopoverTextFieldActivator",
+    title: "Title of my step 3",
+    content: "Content of my step 3",
+    placement: "auto"
+  }
+];
+for (let i = 0; i < steps.length; i++) {
+  let elem = document.querySelector(steps[i].element);
+  elem.setAttribute("data-step", i + 1);
+  elem.setAttribute("data-intro", steps[i].title);
+}
+
+introJs().start();
+
+// Initialize the tour
+// tour.init();
+
+// Start the tour
+// tour.start();
