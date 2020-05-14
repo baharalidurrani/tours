8b2d04cdf74672e46f0bfabd7e8cf8616e9ac30b
baharalidurrani <baharalidurrani@gmail.com>
2020-05-14 16:21:18 -0700

    intro steps() added

--- dep/js/tours/baharIntro2.js
+++ dep/js/tours/baharIntro2.js
@@ -2,21 +2,15 @@ console.log("intro loaded....");
 // Instance the tour
 // var steps = [
 //   {
-//     element: "#PolarisTextField1",
+//     element: "#root > div > div.footer > div.content.center",
 //     title: "Title of my step 1",
 //     content: "Content of my step 1",
 //     placement: "auto"
 //   },
 //   {
-//     element: "html",
+//     element: "#root > div > div.main > div > h1",
 //     title: "Title of my step 2",
 //     content: "Content of my step 2",
-//     placement: "auto"
-//   },
-//   {
-//     element: "#AdjustQuantityPopoverTextFieldActivator",
-//     title: "Title of my step 3",
-//     content: "Content of my step 3",
 //     placement: "auto"
 //   }
 // ];
@@ -25,19 +19,19 @@ console.log("intro loaded....");
 //   elem.setAttribute("data-step", i + 1);
 //   elem.setAttribute("data-intro", steps[i].title);
 // }
+
 introJs().addSteps([
   {
-    element: document.querySelector("#PolarisTextField1"),
+    element: document.querySelector(
+      "#root > div > div.main > div > div > fieldset:nth-child(4) > a:nth-child(2)"
+    ),
     intro: "This is step 1"
   },
   {
-    element: document.querySelector("#product-description_ifr").contentWindow
-      .document.body,
+    element: document.querySelector(
+      "#root > div > div.main > div > div > fieldset:nth-child(4) > a:nth-child(3)"
+    ),
     intro: "This is step 2"
-  },
-  {
-    element: document.querySelector("#AdjustQuantityPopoverTextFieldActivator"),
-    intro: "This is step 3"
   }
 ]);
 introJs().start();
