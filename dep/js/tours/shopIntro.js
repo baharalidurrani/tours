e93a020ca033fceab72ace2928fb57c929a1eb7d
baharalidurrani <baharalidurrani@gmail.com>
2020-05-14 16:14:09 -0700

    intro steps added

--- dep/js/tours/shopIntro.js
+++ dep/js/tours/shopIntro.js
@@ -1,31 +1,45 @@
 console.log("intro loaded....");
 // Instance the tour
-var steps = [
+// var steps = [
+//   {
+//     element: "#PolarisTextField1",
+//     title: "Title of my step 1",
+//     content: "Content of my step 1",
+//     placement: "auto"
+//   },
+//   {
+//     element: "html",
+//     title: "Title of my step 2",
+//     content: "Content of my step 2",
+//     placement: "auto"
+//   },
+//   {
+//     element: "#AdjustQuantityPopoverTextFieldActivator",
+//     title: "Title of my step 3",
+//     content: "Content of my step 3",
+//     placement: "auto"
+//   }
+// ];
+// for (let i = 0; i < steps.length; i++) {
+//   let elem = document.querySelector(steps[i].element);
+//   elem.setAttribute("data-step", i + 1);
+//   elem.setAttribute("data-intro", steps[i].title);
+// }
+introJs.addSteps([
   {
-    element: "#PolarisTextField1",
-    title: "Title of my step 1",
-    content: "Content of my step 1",
-    placement: "auto"
+    element: document.querySelector("#PolarisTextField1"),
+    intro: "This is step 1"
   },
   {
-    element: "html",
-    title: "Title of my step 2",
-    content: "Content of my step 2",
-    placement: "auto"
+    element: document.querySelector("#product-description_ifr").contentWindow
+      .document.body,
+    intro: "This is step 2"
   },
   {
-    element: "#AdjustQuantityPopoverTextFieldActivator",
-    title: "Title of my step 3",
-    content: "Content of my step 3",
-    placement: "auto"
+    element: document.querySelector("#AdjustQuantityPopoverTextFieldActivator"),
+    intro: "This is step 3"
   }
-];
-for (let i = 0; i < steps.length; i++) {
-  let elem = document.querySelector(steps[i].element);
-  elem.setAttribute("data-step", i + 1);
-  elem.setAttribute("data-intro", steps[i].title);
-}
-
+]);
 introJs().start();
 
 // Initialize the tour
